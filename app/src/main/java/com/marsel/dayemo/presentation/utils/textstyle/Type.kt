bb6ec98ec11d0eae8565b28aef620158bdfa32af
арсель амбеталиев <mambetalievmarsel2289@gmail.com>
2025-03-23 23:33:58 +0600

    storage class for custom FontFamily

---
+++ app/src/main/java/com/marsel/dayemo/presentation/utils/textstyle/Type.kt
@@ -0,0 +1,9 @@
+package com.marsel.dayemo.presentation.utils.textstyle
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import com.marsel.dayemo.R
+
+val StapelFontFamily = FontFamily(
+    Font(R.font.stapel_expanded_bold)
+)
